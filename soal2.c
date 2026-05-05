f93a1d4ee77ad885c495df4a4ba2f775767f1e59
1504Daffa <daffarafandytarigan@gmail.com>
2026-05-05 09:43:17 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -2,19 +2,17 @@
 // NIM : 13224068
 // Source Code untuk praktikum modul 4- Dynamics Structure 
 // Soal yang dipilih: Soal Nomor 2
-
 #include <stdio.h>
 #include <string.h>
 
 int main() {
     char s[1000]; //maksimal panjang string 1000 karakter
+    memset(s, 0, sizeof(s)); // inisialisasi string
     if (scanf("%s", s) != 1) {
         return 1;
     }
     int panjang = strlen(s);
-    if (panjang == 0) {
-        return 0;
-    }
+    // Hapus kondisi return 0 untuk panjang == 0 agar tetap output "0"
     int butuhdipasangkan = 0, belumdipasangkan = 0;
 // menghitung jumlah tanda kurung yang belum dipasangkan dan yang sudah dipasangkan
     for (int i = 0; s[i] != '\0'; i++) {
@@ -36,3 +34,5 @@ int main() {
 
     return 0;
 }
+
+
