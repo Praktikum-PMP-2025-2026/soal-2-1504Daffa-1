7185bb97e5919d462679a3459b4b87d91118db92
1504Daffa <daffarafandytarigan@gmail.com>
2026-05-05 09:24:28 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -24,7 +24,7 @@ int main() {
         }
     }
     if (butuhdipasangkan == 0 && belumdipasangkan == 0) {
-        printf("0\n");
+        printf("0");
     } else {
         printf("%d\n", butuhdipasangkan + belumdipasangkan);
     }
